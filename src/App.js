5f5c87ea568fafc3fd982f5bdf6b44cb7a54d71f
DrInnovatio <turing1723@gmail.com>
2021-10-19 18:19:05 +0900

    the second commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import Movie from './components/Movies';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ce213564fafb74b60d0dca1fde29d2f&page=1";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=4ce213564fafb74b60d0dca1fde29d2f&query=";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  } 
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm)
+      setSearchTerm(" ");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const refreshPage = ()=>{
+     window.location.reload();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>
+        <button className="btn" onClick={refreshPage}>Moviness</button>
+        <form onSubmit={ handleOnSubmit }>
+          <input className="search" type="text" placeholder="Search..." value={ searchTerm } onChange={ handleOnChange } />
+        </form>
+    </header>
+    <div className="movie-container">
+     
+      { movies.length > 0 && movies.map((movie) =>
+        <Movie key={movie.id} {...movie}/>)}
+      </div>
+      </>
   );
 }
 
 export default App;
+
+
